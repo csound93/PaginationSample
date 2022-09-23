145b93ccc2045f2bb5fc123ea8ff1e345e4b21f0
csound93 <csound93@gmail.com>
2022-09-23 15:08:37 +0900

    first commit

---
+++ src/main/java/kr/co/netcom/PaginationSampleApplication.java
@@ -0,0 +1,13 @@
+package kr.co.netcom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaginationSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaginationSampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/co/netcom/PaginationSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.netcom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaginationSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
